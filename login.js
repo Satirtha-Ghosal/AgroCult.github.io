1cd0c371af8f29a43eb9efd8d9f411ba3d97bf3e
Satirtha-Ghosal <138100530+Satirtha-Ghosal@users.noreply.github.com>
2023-08-27 09:23:24 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+let go = document.getElementById('login');
+function redirect(){
+    alert("You are successfully logged in")
+    window.location.href = "index.html";
+}
+
+function changeicon(){
+    go.innerHTML ="Logged In"
+}
